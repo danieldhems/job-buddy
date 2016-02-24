7031d98ce68acb5c463e9f36d8b3c5cdaa7025c0
Dan Hems <danhems87@gmail.com>
2016-02-24 09:04:56 +0000

    Remove client directory (no longer in use)

--- client/js/app.js
+++
@@ -1,1 +0,0 @@
-import {ApplicationDispatcher} from './dispatcher';
--- client/js/constants/crud_constants.js
+++
@@ -1,6 +0,0 @@
-export default {
-  CREATE: 'crud.CREATE',
-  READ: 'crud.READ',
-  UPDATE: 'crud.UPDATE',
-  DELETE: 'crud.DELETE'
-};
